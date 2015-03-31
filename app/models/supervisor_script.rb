dcb36c1491a626b493a57d4646e271362e201007
mwrona <michal.wrona.4@gmail.com>
2015-03-31 15:14:10 +0200

    SCAL-636 Improvements in script starting integration test.

--- app/models/supervisor_script.rb
+++ app/models/supervisor_script.rb
@@ -15,9 +15,7 @@ class SupervisorScript < MongoActiveRecord
     config['http_schema'] = 'https' # TODO - temporary, change to config entry
 
     script_config = "/tmp/supervisor_script_config_#{self.experiment_id.to_s}"
-    File.open(script_config, 'w+') {
-        |file| file.write(config.to_json)
-    }
+    File.open(script_config, 'w+') { |file| file.write(config.to_json) }
     script_log = "log/supervisor_script_log_#{self.experiment_id.to_s}"
     path = 'scalarm_supervisor_scrpits/simulated_annealing/anneal.py'
     self.pid = Process.spawn("python2 #{path} #{script_config}", out: script_log, err: script_log)
--- test/integration/script_starting_test.rb
+++ test/integration/script_starting_test.rb
@@ -36,16 +36,24 @@ class ScriptStartingTest < ActionDispatch::IntegrationTest
   SIMULATED_ANNEALING_ID = 'simulated_annealing'
   SCRIPT_LOG_FILE_PATH = "log/supervisor_script_log_#{EXPERIMENT_ID}"
   SCRIPT_CONFIG_FILE_PATH = "/tmp/supervisor_script_config_#{EXPERIMENT_ID}"
-
-  def setup
-    information_service = InformationService.new
-    information_service.expects(:get_list_of).with('experiment_managers').returns([EM_ADDRESS])
-    InformationService.expects(:new).returns(information_service)
-    super
-  end
-
+  SCRIPT_MAIN_FILE = 'scalarm_supervisor_scrpits/simulated_annealing/anneal.py'
+  SCRIPT_LIBRARY_FILE = 'scalarm_supervisor_scrpits/simulated_annealing/scalarmapi.py'
+  PID = 1
 
   test "starting simmulated annealing supervisor script" do
+    # mocks
+    information_service = InformationService.new
+    information_service.expects(:get_list_of).with('experiment_managers').returns([EM_ADDRESS]).once
+    InformationService.expects(:new).returns(information_service).once
+
+    Process.expects(:spawn).with("python2 #{SCRIPT_MAIN_FILE} #{SCRIPT_CONFIG_FILE_PATH}",
+                                 out: SCRIPT_LOG_FILE_PATH, err: SCRIPT_LOG_FILE_PATH).returns(PID).once
+    Process.expects(:detach).with(PID).once
+    # test
+
+    assert File.exists? SCRIPT_MAIN_FILE
+    assert File.exists? SCRIPT_LIBRARY_FILE
+
     assert_difference 'SupervisorScript.count', 1 do
       post start_supervisor_script_path script_id: SIMULATED_ANNEALING_ID, config: CONFIG_FROM_EM.to_json
     end
@@ -54,18 +62,14 @@ class ScriptStartingTest < ActionDispatch::IntegrationTest
       response_hash.assert_valid_keys('status', 'pid')
     end
     assert_equal response_hash['status'], 'ok'
-    assert File.exists? SCRIPT_LOG_FILE_PATH
+    assert_equal response_hash['pid'], PID
     assert File.exists? SCRIPT_CONFIG_FILE_PATH
     assert_equal FULL_CONFIG.to_json, File.read(SCRIPT_CONFIG_FILE_PATH)
-    @pid = response_hash['pid']
   end
 
   def teardown
-    File.delete SCRIPT_LOG_FILE_PATH if File.exists? SCRIPT_LOG_FILE_PATH
     File.delete SCRIPT_CONFIG_FILE_PATH if File.exists? SCRIPT_CONFIG_FILE_PATH
-    `kill -9 #{@pid}` if `ps #{@pid} | wc -l` == '2'
-    @pid = nil
-    # super
+    super
   end
 
 
