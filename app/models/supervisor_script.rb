ca48bc5a5c6124b736584ca2f913d28ef9b970d6
mwrona <michal.wrona.4@gmail.com>
2015-04-03 18:47:11 +0200

    SCAL-636 Hashes in doc for links to methods

--- app/models/supervisor_script.rb
+++ app/models/supervisor_script.rb
@@ -3,9 +3,10 @@
 # its creation, monitoring and deletion.
 #
 # List of possible attributes:
-# * experiment_id - id of experiment that is supervised by script (sets by start method)
-# * script_id - id of supervisor script, specify which script is used for supervising (sets by start method)
-# * pid - pid of supervisor script process (sets by start method)
+# * experiment_id - id of experiment that is supervised by script (sets by #start method)
+# * script_id - id of supervisor script, specify which script is used for supervising (sets by #start
+#   method)
+# * pid - pid of supervisor script process (sets by #start method)
 class SupervisorScript < MongoActiveRecord
 
   ##
