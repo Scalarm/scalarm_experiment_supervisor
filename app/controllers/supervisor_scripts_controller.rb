9676c19f4f292c359caa3caacc4c12dd9634ee46
mwrona <michal.wrona.4@gmail.com>
2015-04-04 14:45:52 +0200

    SCAL-636 apidocjs documentation of start_supervisor_script and typo fix in controller name.

---
+++ app/controllers/supervisor_scripts_controller.rb
@@ -0,0 +1,79 @@
+require 'json'
+
+class SupervisorScriptsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def new
+  end
+
+=begin
+    @api {post} /start_supervisor_script.json Start Supervisor Script
+    @apiName start_supervisor_script
+    @apiGroup SupervisorScripts
+    @apiDescription This action allows user to start new supervisor script with given parameters.
+
+    @apiParam {String} script_id Mandatory, id of supervisor script to be started
+    @apiParam {Object} config Mandatory, json object with config of supervisor script
+    @apiParam {String} config.experiment_id Mandatory, id of experiment to be managed
+    @apiParam {String} config.user Mandatory, username used in authentication in Experiment Manager
+    @apiParam {String} config.password Mandatory, password used in authentication in Experiment Manager
+    @apiParam {Number[]} config.lower_limits Mandatory, lower boundary of experiment input space
+    @apiParam {Number[]} config.upper_limits Mandatory, upper boundary of experiment input space
+    @apiParam {String[]} config.parameter_ids Mandatory, ids of input space parameter in correct order
+    @apiParam {Number[]} config.start_point Mandatory, start point of supervisor script
+
+    @apiParamExample Params-Example
+    script_id: 'simulated annealing'
+    config:
+    {
+      maxiter: 1,
+      dwell: 1,
+      schedule: 'boltzmann',
+      experiment_id: '551fca1f2ab4f259fc000002',
+      user: 'user',
+      password: 'password',
+      lower_limit: [-2, -3],
+      upper_limit: [2, 3],
+      parameters_ids: [c___g___x, c___g___y],
+      start_point: [0, 0]
+    }
+
+    @apiSuccess {Object} info json object with information about performed action
+    @apiSuccess {String} info.status status of performed action, on success always 'ok'
+    @apiSuccess {Number} info.pid pid of supervisor script managing experiment
+
+    @apiSuccessExample {json} Success-Response
+    {
+      'status': 'ok'
+      'pid': 1234
+    }
+
+    @apiError {Object} info json object with information about performed action
+    @apiError {String} info.status status of performed action, on failure always 'error'
+    @apiError {String} info.reason reason of failure to start supervisor script
+
+    @apiErrorExample {json} Failure-Response
+    {
+        'status': 'error'
+        'reason': 'Unable to locate supervisor script files'
+    }
+=end
+  def create
+    #TODO validate and check errors
+    response = {}
+    begin
+      supervisor_script = SupervisorScript.new({})
+      pid = supervisor_script.start params[:script_id], JSON.parse(params[:config])
+      supervisor_script.save
+      Rails.logger.debug supervisor_script
+      response = {status: 'ok', pid: pid}
+
+    rescue StandardError => e
+      Rails.logger.debug("Error while starting new supervisor script: #{e.to_s}")
+      response.merge!({status: 'error', reason: "[Experiment Supervisor] #{e.to_s}"})
+      supervisor_script.destroy
+    end
+
+    render json: response
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'start_supervisor_script' => 'supervisor_script#new'
-  post 'start_supervisor_script' => 'supervisor_script#create'
+  get 'start_supervisor_script' => 'supervisor_scripts#new'
+  post 'start_supervisor_script' => 'supervisor_scripts#create'
 
 end
