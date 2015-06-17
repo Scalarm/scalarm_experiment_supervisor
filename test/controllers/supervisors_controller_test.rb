fdd31d36177ffb68634940ec2af7d152185b7a18
mpaciore <mateusz.j.paciorek@gmail.com>
2015-06-17 10:36:32 +0200

    SCAL-769 First try on tests

--- test/controllers/supervisors_controller_test.rb
+++ test/controllers/supervisors_controller_test.rb
@@ -3,7 +3,9 @@ require 'json'
 
 class SupervisorsControllerTest < ActionController::TestCase
 
-  MANIFEST = {'test' => 'test', 'test2' => ['foo', 'bar', 42]}
+  PUBLIC_MANIFEST = {'test' => 'test', 'test2' => ['foo', 'bar', 42], 'public' => true}
+  NON_PUBLIC_MANIFEST_EXPL = {'a' => 'a', 'bc3' => ['b', 'c', 3], 'public' => false}
+  RESTRICTED_MANIFEST_IMPL = {'a' => 'a', 'bc3' => ['b', 'c', 3]}
   ID = 'test'
   VIEW_TEST_FILE = Rails.root.join('supervisors', 'views', "#{ID}.html")
 
@@ -16,17 +18,17 @@ class SupervisorsControllerTest < ActionController::TestCase
   end
 
   test 'index should return manifest of supervisors' do
-    Supervisor.expects(:get_manifests).returns([MANIFEST])
+    Supervisor.expects(:get_manifests).returns([PUBLIC_MANIFEST])
     get :index
     manifest = JSON.parse(response.body)
-    assert_equal manifest, [MANIFEST]
+    assert_equal manifest, [PUBLIC_MANIFEST]
   end
 
   test 'show should return manifest of given supervisor id' do
-    Supervisor.expects(:get_manifest).with(ID).returns(MANIFEST)
+    Supervisor.expects(:get_manifest).with(ID).returns(PUBLIC_MANIFEST)
     get :show, id: ID
     manifest = JSON.parse(response.body)
-    assert_equal manifest, MANIFEST
+    assert_equal manifest, PUBLIC_MANIFEST
   end
 
   test 'show should redirect to index on non existing id (html)' do
