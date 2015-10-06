4e395a8cbd12e94cae39d4604e355b145c886a0f
Michał Wrona <michal.wrona.4@gmail.com>
2015-10-06 08:47:37 +0200

    SCAL-1026 Removing redundant code.
    https://jira.plgrid.pl/jira/browse/SCAL-1026

--- supervisors/simulated_annealing/scalarmapi.py
+++ supervisors/simulated_annealing/scalarmapi.py
@@ -1,13 +1,7 @@
 import json
-import math
 import requests
 from requests.auth import HTTPBasicAuth
 import time
-
-f1 = lambda x: (4 - 2.1 * x[0] ** 2 + (x[0] ** 4) / 3) * x[0] ** 2 + x[0] * x[1] + (-4 + 4 * x[1] ** 2) * x[1] ** 2
-
-f2 = lambda x: math.e + 20 - 20 * math.exp(-0.2 * math.sqrt(0.5 * (x[0] ** 2 + x[1] ** 2))) \
-               - math.exp(0.5 * (math.cos(2 * math.pi * x[0]) + math.cos(2 * math.pi * x[1])))
 
 SLEEP_DURATION_BETWEEN_QUERIES = 5  # seconds
 ERROR_OCCURRENCES_BETWEEN_WARNING = 10
