d46ee684ca54ca716a057028bb74d2cf9b1ad9e3
Zuchens <paulina.zak1@gmail.com>
2015-10-19 15:19:33 +0200

    SCAL-879: renamed warning, resolved problem with haml

--- supervisors/sensitivity_analysis_morris/sensitivity_analysis_morris_executor.rb
+++ supervisors/sensitivity_analysis_morris/sensitivity_analysis_morris_executor.rb
@@ -2,7 +2,7 @@ require 'supervisor_executors/abstract_supervisor_executor'
 
 class SensitivityAnalysisMorrisExecutor < AbstractSupervisorExecutor
 
-  SCRIPT_PATH =  File.join(File.dirname(__FILE__),"morris.R")
+  SCRIPT_PATH = File.join(File.dirname(__FILE__),"morris.R")
 
   # overrides parent method
   def self.start(config)
@@ -10,11 +10,7 @@ class SensitivityAnalysisMorrisExecutor < AbstractSupervisorExecutor
     script_config = config_file_path(experiment_id)
     File.open(script_config, 'w+') { |file| file.write(config.to_json) }
     script_log = self.log_path(experiment_id).to_s
-
-
     pid = Process.spawn("Rscript", SCRIPT_PATH, script_config, out: script_log, err: script_log)
-
-
     # does not find a path to file
     Process.detach(pid)
     pid
