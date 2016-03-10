1cc7a954c3f9b75bd12afcb1db325b872c29388e
Jakub Liput <jakub.liput@gmail.com>
2016-03-10 09:46:30 +0100

    Adding libcurl4-openssl-dev dependency for sensitivity analysis

--- supervisors/sensitivity_analysis/install.sh
+++ supervisors/sensitivity_analysis/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 THIS_DIR=`dirname "$0"`
-sudo apt-get -y install r-base r-base-dev r-cran-rjson
+sudo apt-get -y install r-base r-base-dev r-cran-rjson libcurl4-openssl-dev
 pushd ${THIS_DIR}
     sudo Rscript ./install_r_packages.R
 popd
